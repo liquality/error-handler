d14b29ca6c31b6c55913c199b421b34659885429
koderholic <22542946+koderholic@users.noreply.github.com>
2022-09-02 23:17:57 +0100

    fix: resolve type error causing test to fail

--- src/parsers/1inchApiErrorParser.ts
+++ src/parsers/1inchApiErrorParser.ts
@@ -10,29 +10,29 @@ export class OneInchAPIErrorParser implements ErrorParser<OneInchSourceError> {
         let devMsg = ""
         let errorType: ErrorType
 
-        switch (error.description) {
-            case "Insufficient liquidity":
+        switch (error?.description?.toLowerCase()) {
+            case "Insufficient liquidity".toLowerCase():
                 errorType = ErrorType.InsufficientLiquidity
                 break;
-            case "Cannot estimate":
+            case "Cannot estimate".toLowerCase():
                 errorType = ErrorType.QuoteError
-                devMsg = "1inch could not estimate a quote for the swap"
+                devMsg = "1inch could not estimate a quote for the swap".toLowerCase()
                 break;
-            case "You may not have enough ETH balance for gas fee":
+            case "You may not have enough ETH balance for gas fee".toLowerCase():
                 errorType = ErrorType.InsufficientGasFee
                 userMsg = ErrorMessages.InsufficientGasFee()
                 break;
-            case "FromTokenAddress cannot be equals to toTokenAddress":
+            case "FromTokenAddress cannot be equals to toTokenAddress".toLowerCase():
                 errorType = ErrorType.InternalError
                 break;
-            case "Cannot estimate. Don't forget about miner fee. Try to leave the buffer of ETH for gas":
+            case "Cannot estimate. Don't forget about miner fee. Try to leave the buffer of ETH for gas".toLowerCase():
                 errorType = ErrorType.QuoteError
                 devMsg = "1inch internal error, could be related to slippage for specific tokens"
                 break;
-            case "Not enough balance":
+            case "Not enough balance".toLowerCase():
                 errorType = ErrorType.InsufficientFunds
                 break;
-            case "Not enough allowance":
+            case "Not enough allowance".toLowerCase():
                 errorType = ErrorType.InternalError
                 devMsg = "Check the approval process for 1inch, approvals are not being made correctly"
                 break;
